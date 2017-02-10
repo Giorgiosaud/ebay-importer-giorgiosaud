72a0704a4671f4aebe589d6a7d741eed18c1f207
Jorge Saud <jorgelsaud@gmail.com>
2017-02-09 22:41:38 -0400

    fix bug

--- admin/partials/ebay-importer-giorgiosaud-admin-display.php
+++ admin/partials/ebay-importer-giorgiosaud-admin-display.php
@@ -16,22 +16,22 @@
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
 <div class="wrap">
 	<h1>Ebay Importer Setup</h1>
-	<form method="post" action="options.php"> 
+	<form method="post"> 
 		<?php settings_fields( 'ebay_importer_group' );
 		do_settings_sections( 'ebay_importer_group' );?>
 		<table class="form-table">
 			<tr valign="top">
-				<th scope="row"><?php _e('Ebay API Name') ?></th>
+				<th scope="row"><?php _e('Ebay APP ID (Client ID)') ?></th>
 				<td><input type="text" name="new_option_name" value="<?php echo esc_attr( get_option('ebay_api_name') ); ?>" /></td>
 			</tr>
 
 			<tr valign="top">
-				<th scope="row"><?php _e('Ebay API Key') ?></th>
+				<th scope="row"><?php _e('Dev ID') ?></th>
 				<td><input type="text" name="some_other_option" value="<?php echo esc_attr( get_option('ebay_api_key') ); ?>" /></td>
 			</tr>
 
 			<tr valign="top">
-				<th scope="row"><?php _e('Ebay API Secret') ?></th>
+				<th scope="row"><?php _e('Cert ID (Client Secret)') ?></th>
 				<td><input type="text" name="option_etc" value="<?php echo esc_attr( get_option('ebay_api_key_secret') ); ?>" /></td>
 			</tr>
 		</table>
