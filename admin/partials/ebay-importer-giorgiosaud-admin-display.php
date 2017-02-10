2f037731a3b0d07f21b046345e8b4762ad1aa792
Jorge Saud <jorgelsaud@gmail.com>
2017-02-09 21:54:26 -0400

    add ebay api field

--- admin/partials/ebay-importer-giorgiosaud-admin-display.php
+++ admin/partials/ebay-importer-giorgiosaud-admin-display.php
@@ -15,11 +15,28 @@
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
 <div class="wrap">
-<h1>Ebay Importer Setup</h1>
-<form method="post" action="options.php"> 
-<?php settings_fields( 'ebay-api' );
-do_settings_sections( 'ebay-api' );
-submit_button(); ?>
-</form>
+	<h1>Ebay Importer Setup</h1>
+	<form method="post" action="options.php"> 
+		<?php settings_fields( 'ebay-importer-group' );
+		do_settings_sections( 'ebay-importer-group' );?>
+		<table class="form-table">
+			<tr valign="top">
+				<th scope="row">New Option Name</th>
+				<td><input type="text" name="new_option_name" value="<?php echo esc_attr( get_option('new_option_name') ); ?>" /></td>
+			</tr>
+			
+			<tr valign="top">
+				<th scope="row">Some Other Option</th>
+				<td><input type="text" name="some_other_option" value="<?php echo esc_attr( get_option('some_other_option') ); ?>" /></td>
+			</tr>
+			
+			<tr valign="top">
+				<th scope="row">Options, Etc.</th>
+				<td><input type="text" name="option_etc" value="<?php echo esc_attr( get_option('option_etc') ); ?>" /></td>
+			</tr>
+		</table>
+		<?
+		submit_button(); ?>
+	</form>
 </div>
 
