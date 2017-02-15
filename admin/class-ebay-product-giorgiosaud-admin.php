fd45bd86c68623b5e85e34f02b83b4b527da8988
Jorge Saud <jorgelsaud@gmail.com>
2017-02-15 19:09:09 -0400

    add product class to manage product

--- admin/class-ebay-product-giorgiosaud-admin.php
+++ admin/class-ebay-product-giorgiosaud-admin.php
@@ -1,11 +1,14 @@
 <?php
 class EbayProductGiorgiosaud{
 	private $xml;
+	private $id;
 
-	public function __construct($xml)
+	public function __construct($xml, $id)
 	{
 		$this->xml = $xml;
-		dd($this);
+		$this->id = $this->xml->itemId->__toString;
+		dd($this->id);
+
 	}
 
 }
