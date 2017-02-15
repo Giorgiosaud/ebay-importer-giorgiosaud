07aa462d8195da6d3a19a1315b4e61043041c218
Jorge Saud <jorgelsaud@gmail.com>
2017-02-15 15:26:57 -0400

    testing the api

--- admin/class-ebay-importer-giorgiosaud-admin.php
+++ admin/class-ebay-importer-giorgiosaud-admin.php
@@ -341,7 +341,7 @@ class Ebay_Importer_Giorgiosaud_Admin {
 		$store = 'expomiamiautoparts';                  
 		$resp = $this->getProductsByStore($store);
 		// Check to see if the call was successful, else print an error
-		// var_dump($resp->ack == "Success");
+		die(var_dump($resp));
 		if ($resp->ack == "Success") {
   			// Initialize the $results variable
   			// var_dump($resp);
