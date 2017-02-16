1039feda9d50e1a99d4094e517bfbb5af0d45bef
Jorge Saud <jorgelsaud@gmail.com>
2017-02-16 07:52:32 -0400

    test to get description

--- admin/class-ebay-product-giorgiosaud-admin.php
+++ admin/class-ebay-product-giorgiosaud-admin.php
@@ -13,12 +13,14 @@ class EbayProductGiorgiosaud{
 	protected function parseXML(){
 		$this->eBayId = $this->xml->ItemID->__toString();
 		$this->title = $this->xml->Title->__toString();
-		$this->description=$text = preg_replace('#(<[a-z ]*)(style=("|\')(.*?)("|\'))([a-z ]*>)#', '\\1\\6', strip_tags( $this->xml->Description->__toString(),'<p>'));
-;
 		$this->eBayUrl=$this->xml->ViewItemURLForNaturalSearch->__toString();
+		$this->description=$text = preg_replace('#(<[a-z ]*)(style=("|\')(.*?)("|\'))([a-z ]*>)#', '\\1\\6', strip_tags( $this->xml->Description->__toString(),'<a>'));
 		echo '<pre>';
-		var_dump($this);
+		var_dump($this->description);
 		echo '</pre>';
+		// echo '<pre>';
+		// var_dump($this);
+		// echo '</pre>';
 	}
 
 }
