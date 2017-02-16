f6c642a81589c681a3a9ce2e60ebaa5dacb268fa
Jorge Saud <jorgelsaud@gmail.com>
2017-02-16 09:34:51 -0400

    slugify

--- admin/class-ebay-product-giorgiosaud-admin.php
+++ admin/class-ebay-product-giorgiosaud-admin.php
@@ -54,7 +54,7 @@ class EbayProductGiorgiosaud{
 			$val=$specifics->Value->__toString();
 			$this->specifications->{$name}=$val;
 		}
-		die('<pre>'.var_dump($this->specifications->manufacturerpartnumber).'</pre>');
+		// die('<pre>'.var_dump($this->specifications->manufacturerpartnumber).'</pre>');
 		$this->mainPicture=substr($picurl,0,strpos( $picurl, 'JPG' )+3);
 		// die(var_dump($this->mainPicture));
 		$this->description=$text = preg_replace('#(<[a-z ]*)(style=("|\')(.*?)("|\'))([a-z ]*>)#', '\\1\\6', strip_tags( $this->xml->Description->__toString(),'<a>'));
