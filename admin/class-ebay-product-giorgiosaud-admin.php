c27aae2dece05e8ce89f7a4df57f90d7a8505f21
Jorge Saud <jorgelsaud@gmail.com>
2017-02-15 20:20:16 -0400

    remove style from description

--- admin/class-ebay-product-giorgiosaud-admin.php
+++ admin/class-ebay-product-giorgiosaud-admin.php
@@ -13,7 +13,8 @@ class EbayProductGiorgiosaud{
 	protected function parseXML(){
 		$this->eBayId = $this->xml->ItemID->__toString();
 		$this->title = $this->xml->Title->__toString();
-		$this->description=$this->xml->Description->__toString();
+		$this->description=$text = preg_replace('#(<[a-z ]*)(style=("|\')(.*?)("|\'))([a-z ]*>)#', '\\1\\6', $this->xml->Description->__toString());
+;
 		$this->eBayUrl=$this->xml->ViewItemURLForNaturalSearch->__toString();
 		echo '<pre>';
 		var_dump($this);
