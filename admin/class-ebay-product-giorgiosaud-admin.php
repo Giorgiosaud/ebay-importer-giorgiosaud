a477b40a65cda7dded080ca09b6339a9dd8dbe8a
Jorge Saud <jorgelsaud@gmail.com>
2017-02-16 11:32:29 -0400

    fix issue

--- admin/class-ebay-product-giorgiosaud-admin.php
+++ admin/class-ebay-product-giorgiosaud-admin.php
@@ -87,8 +87,7 @@ class EbayProductGiorgiosaud{
 		}
 
 		$this->compatibilityTitles=array_keys((array)$this->compatibility[0]);
-		dd($this->title);
-
+		// dd($this->title);
 	}
 	/* Import media from url
  *
