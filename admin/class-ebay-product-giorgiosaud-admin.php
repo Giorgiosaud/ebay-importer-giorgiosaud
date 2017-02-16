5c9922f5b6437c5814b2a5f30e07b02540122cb4
Jorge Saud <jorgelsaud@gmail.com>
2017-02-16 09:02:31 -0400

    test mypicture

--- admin/class-ebay-product-giorgiosaud-admin.php
+++ admin/class-ebay-product-giorgiosaud-admin.php
@@ -17,6 +17,7 @@ class EbayProductGiorgiosaud{
 		$this->eBayUrl=$this->xml->ViewItemURLForNaturalSearch->__toString();
 
 		$this->mainPicture=$this->xml->PictureURL[0]->__toString();
+		var_dump($this->mainPicture);
 		$this->description=$text = preg_replace('#(<[a-z ]*)(style=("|\')(.*?)("|\'))([a-z ]*>)#', '\\1\\6', strip_tags( $this->xml->Description->__toString(),'<a>'));
 		echo '<pre>';
 		var_dump($this->description);
