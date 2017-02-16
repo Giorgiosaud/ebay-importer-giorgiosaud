8754eaa63f88912f7aaa8b8b6538b62585ee2ed2
Jorge Saud <jorgelsaud@gmail.com>
2017-02-16 10:36:03 -0400

    test html

--- admin/class-ebay-importer-giorgiosaud-admin.php
+++ admin/class-ebay-importer-giorgiosaud-admin.php
@@ -353,7 +353,7 @@ class Ebay_Importer_Giorgiosaud_Admin {
 				<a href="<?= $product->eBayUrl?>"><h1><?= $product->title ?></h1></a>
 				<p>Code: <span><?= $product->eBayId ?></span></p>
 				?>
-				<img src="<?= $product->mainPicture?>" alt="<?= $product->title ?>">
+				<img src="<?= $product->mainPicture ?>" alt="<?= $product->title ?>">
 				<p>
 					<?= $product->description ?>
 				</p>
