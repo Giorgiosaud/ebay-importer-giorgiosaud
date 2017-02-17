91d6525654cc5463e6451ec712927fde3905f25e
Jorge Saud <jorgelsaud@gmail.com>
2017-02-17 10:57:59 -0400

    test to get compatibles list

--- admin/class-ebay-product-giorgiosaud-admin.php
+++ admin/class-ebay-product-giorgiosaud-admin.php
@@ -68,6 +68,7 @@ class EbayProductGiorgiosaud{
 			$val=$specifics->Value->__toString();
 			$this->specifications->{$name}=$val;
 		}
+		dd($this->xml);
 		foreach($this->xml->ItemCompatibilityList as $compatibilityList){
 			dd($compatibilityList["Compatibility"]);
 			
