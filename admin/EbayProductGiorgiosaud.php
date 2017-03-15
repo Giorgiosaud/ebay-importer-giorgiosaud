30c973d35bb3e8193ee2e5873c73d489ed993d81
Jorge Saud <jorgelsaud@gmail.com>
2017-03-15 17:36:09 -0400

    test table

--- admin/EbayProductGiorgiosaud.php
+++ admin/EbayProductGiorgiosaud.php
@@ -15,29 +15,32 @@ class EbayProductGiorgiosaud{
 	}
 	function display(){
 		?>
-		<table class="wp-list-table widefat fixed striped posts">
-			<thead>
-				<tr>
-					<th scope="col" id="ebay_id" class="manage-column column-primary">
-						<span>Ebay ID</span>
-					</th>
-					<th scope="col" id="name" class="manage-column ">Name</th>
-					<th scope="col" id="url" class="manage-column column-url">URL</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php foreach($this->productos as $producto){
-					?>
-					<tr id="<?=producto['ID']?>" class="iedit author-self level-0 type-post status-publish format-standard hentry category-sport category-workshop tag-bike tag-race tag-speed">
-						<td class="id column-id" data-colname="ID"><?=producto['ID']?></td>
-						<td class="id column-name" data-colname="Name"><?=producto['Name']?></td>
-						<td class="id column-URL" data-colname="URL"><?=producto['URL']?></td>
+		<div class="wrap">
+		<h1 class="wp-heading-inline">Ebay Products</h1>
+			<table class="wp-list-table widefat fixed striped posts">
+				<thead>
+					<tr>
+						<th scope="col" id="ebay_id" class="manage-column column-primary">
+							<span>Ebay ID</span>
+						</th>
+						<th scope="col" id="name" class="manage-column ">Name</th>
+						<th scope="col" id="url" class="manage-column column-url">URL</th>
 					</tr>
-					<?php
-				}?>
-			}
-		</tbody>
-	</table>
+				</thead>
+				<tbody>
+					<?php foreach($this->productos as $producto){
+						?>
+						<tr id="<?=$producto['ID']?>" class="iedit author-self level-0 type-post status-publish format-standard hentry category-sport category-workshop tag-bike tag-race tag-speed">
+							<td class="id column-id" data-colname="ID"><?=$producto['ID']?></td>
+							<td class="id column-name" data-colname="Name"><?=$producto['Name']?></td>
+							<td class="id column-URL" data-colname="URL"><?=$producto['URL']?></td>
+						</tr>
+						<?php
+					}?>
+				}
+			</tbody>
+		</table>
+	</div>
 	<?php
 
 }
