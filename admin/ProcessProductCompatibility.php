a822f266c596875b0b46076734b1d858a87bc2ab
Jorge Saud <jorgelsaud@gmail.com>
2017-03-16 15:06:17 -0400

    handle with post requesto

--- admin/ProcessProductCompatibility.php
+++ admin/ProcessProductCompatibility.php
@@ -3,12 +3,25 @@ class ProcessProductCompatibility{
 	private $idEbay;
 	private $details;
 
-	public function __construct($idEbay, $details){
+	public function __construct($idEbay, $xmlstring){
 
 		$this->idEbay = $idEbay;
-		$this->details = $details;
+		$this->details = $this->SimpleXML2Array($xmlstring);
+		
 	}
 	public function newOrUpdateCompatibility(){
 		echo $this->details;
+	}
+	static public function SimpleXML2Array($xml){
+		$array = (array)$xml;
+
+    //recursive Parser
+		foreach ($array as $key => $value){
+			if(strpos(get_class($value),"SimpleXML")!==false){
+				$array[$key] = SimpleXML2Array($value);
+			}
+		}
+
+		return $array;
 	}
 }
