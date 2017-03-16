e9a541ef16edbf32e9edc941f791c0058d13186e
Jorge Saud <jorgelsaud@gmail.com>
2017-03-16 16:57:33 -0400

    fix ebay id custom field

--- admin/ProcessProductCompatibility.php
+++ admin/ProcessProductCompatibility.php
@@ -18,52 +18,9 @@ class ProcessProductCompatibility{
 		if ( $query->have_posts() ) {
 			while ( $query->have_posts() ) {
 				$query->the_post();
-				$header=array();
-				$headerElement=$this->details->ItemCompatibilityList->Compatibility[0];
-				foreach($headerElement->NameValueList as $listElement){
-					if($listElement->Name!=''){
-						$column=array('c'=>$listElement->Name->__toString());
-						array_push($header,$column);
-					}
-
-				}
-				$column=array(
-					'c'=>'Compatibility Notes');
-				array_push($header,$column);
-				$table=array();
-				
-				$body=array();
-				// var_dump(get_field('compatible_table'));
-				// die();
-				foreach($this->details->ItemCompatibilityList->Compatibility as $compatible){
-					$linea=array();
-					foreach($compatible->NameValueList as $listElement){
-						if($listElement->Value!=''){
-							$elemento=array('c'=>$listElement->Value->__toString());
-							array_push($linea,$elemento);
-						}
-					}
-					$elemento=array('c'=>$compatible->CompatibilityNotes->__toString());
-					array_push($linea,$elemento);
-
-					// var_dump($linea);
-					array_push($body,$linea);					
-					
-				}	
-				$table['p']['o']['uh']=1;
-				$table['c']=array();
-				foreach($header as $head)
-				{
-					array_push($table['c'],array('p'=>''));
-				}				
-				$table['h']=$header;
-				$table['b']=$body;
-
-				
-				update_field('compatible_table', json_encode($table), $query->post->ID);
-				die();
-
-				
+				$this->updateCompatibilityTable($query->post->ID);
+				wp_redirect(edit_post_link(null,null,null,$query->post->ID));
+				exit;
 			}
 				// var_dump($this->details->ItemCompatibilityList);
 				// die();
@@ -75,15 +32,62 @@ class ProcessProductCompatibility{
 			die();
 			global $user_ID;
 			$new_post = array(
-				'post_title' => 'My New Post',
-				'post_content' => 'Lorem ipsum dolor sit amet...',
-				'post_status' => 'publish',
+				'post_title' => $this->details->Title->__toString(),
+				'post_content' => $this->details->ConditionDescription->__toString(),
+				'post_status' => 'draft',
 				'post_date' => date('Y-m-d H:i:s'),
 				'post_author' => $user_ID,
-				'post_type' => 'post',
+				'post_type' => 'product',
 				'post_category' => array(0)
 				);
 			$post_id = wp_insert_post($new_post);
+			$this->updateCompatibilityTable($post_id);
 		}
+	}
+	public function updateCompatibilityTable($id){
+		$header=array();
+		$headerElement=$this->details->ItemCompatibilityList->Compatibility[0];
+		foreach($headerElement->NameValueList as $listElement){
+			if($listElement->Name!=''){
+				$column=array('c'=>$listElement->Name->__toString());
+				array_push($header,$column);
+			}
+
+		}
+		$column=array(
+			'c'=>'Compatibility Notes');
+		array_push($header,$column);
+		$table=array();
+
+		$body=array();
+				// var_dump(get_field('compatible_table'));
+				// die();
+		foreach($this->details->ItemCompatibilityList->Compatibility as $compatible){
+			$linea=array();
+			foreach($compatible->NameValueList as $listElement){
+				if($listElement->Value!=''){
+					$elemento=array('c'=>$listElement->Value->__toString());
+					array_push($linea,$elemento);
+				}
+			}
+			$elemento=array('c'=>$compatible->CompatibilityNotes->__toString());
+			array_push($linea,$elemento);
+
+					// var_dump($linea);
+			array_push($body,$linea);					
+
+		}	
+		$table['p']['o']['uh']=1;
+		$table['c']=array();
+		foreach($header as $head)
+		{
+			array_push($table['c'],array('p'=>''));
+		}				
+		$table['h']=$header;
+		$table['b']=$body;
+
+
+		update_field('compatible_table', json_encode($table), $id);
+
 	}
 }
