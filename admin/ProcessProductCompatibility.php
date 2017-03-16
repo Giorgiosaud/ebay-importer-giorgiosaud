bc2f8bf8a592e1a38ebe96fee69a163a53bf860b
Jorge Saud <jorgelsaud@gmail.com>
2017-03-16 15:24:01 -0400

    handle with post requesto

--- admin/ProcessProductCompatibility.php
+++ admin/ProcessProductCompatibility.php
@@ -21,7 +21,7 @@ class ProcessProductCompatibility{
 		if ( $query->have_posts() ) {
 			while ( $query->have_posts() ) {
 				$query->the_post();
-				echo $query->post->ID;
+				echo 'Product Id '.$query->post->ID;
 			}
 		}
 		else{
