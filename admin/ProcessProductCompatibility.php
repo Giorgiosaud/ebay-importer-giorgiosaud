a708bd27d9ac0566b46f1677c077f677bb51dd18
Jorge Saud <jorgelsaud@gmail.com>
2017-03-16 15:40:13 -0400

    fix ebay id custom field

--- admin/ProcessProductCompatibility.php
+++ admin/ProcessProductCompatibility.php
@@ -18,7 +18,7 @@ class ProcessProductCompatibility{
 		if ( $query->have_posts() ) {
 			while ( $query->have_posts() ) {
 				$query->the_post();
-				var_dump($this->details->ItemCompatibilityList);
+				var_dump($this->details->ItemCompatibilityList->Compatibility);
 				die();
 				var_dump(get_field('compatible_table'));
 				// echo 'Product Id '.$query->post->ID;
